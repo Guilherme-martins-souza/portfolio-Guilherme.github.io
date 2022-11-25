be21a2f7b4f82be8f8e79c8876c252e4d4047833
Guilherme <logingui@gmail.com>
2022-11-25 09:47:46 -0300

    botao para o topo ainda não finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("arroz");
+
+const topo = document.querySelector(".scrollTop");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 500) {
+        topo.classList.add("active");
+    } else {
+        topo.classList.remove("active");
+    }
+});
