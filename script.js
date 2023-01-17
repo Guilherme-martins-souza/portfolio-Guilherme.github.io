6a4765c8c85f61330cc684ec3e605de722198550
Guilherme <logingui@gmail.com>
2023-01-17 15:11:43 -0300

    organizando pasta

--- script.js
+++
@@ -1,9 +0,0 @@
-const topo = document.querySelector(".scrollTop");
-
-window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 500) {
-        topo.classList.add("active");
-    } else {
-        topo.classList.remove("active");
-    }
-});
